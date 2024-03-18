c5fe4d30361eea129cd4924741bf81087801987d
74731Charushila <127972443+74731Charushila@users.noreply.github.com>
2024-03-18 12:58:29 +0530

    Add files via upload

---
+++ Controllers/MenusController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hotel_Management_System.Models;
+
+namespace Hotel_Management_System.Controllers
+{
+    public class MenusController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenusController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Menus
+        public async Task<IActionResult> Index()
+        {
+              return _context.Menulist != null ? 
+                          View(await _context.Menulist.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Menulist'  is null.");
+        }
+
+        // GET: Menus/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Menulist == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menulist
+                .FirstOrDefaultAsync(m => m.MenuId == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // GET: Menus/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Menus/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MenuId,MenuName,Menu_Type,Menu_Description,Menu_Price")] Menu menu)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(menu);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: Menus/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Menulist == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menulist.FindAsync(id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+            return View(menu);
+        }
+
+        // POST: Menus/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MenuId,MenuName,Menu_Type,Menu_Description,Menu_Price")] Menu menu)
+        {
+            if (id != menu.MenuId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(menu);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MenuExists(menu.MenuId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(menu);
+        }
+
+        // GET: Menus/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Menulist == null)
+            {
+                return NotFound();
+            }
+
+            var menu = await _context.Menulist
+                .FirstOrDefaultAsync(m => m.MenuId == id);
+            if (menu == null)
+            {
+                return NotFound();
+            }
+
+            return View(menu);
+        }
+
+        // POST: Menus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Menulist == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Menulist'  is null.");
+            }
+            var menu = await _context.Menulist.FindAsync(id);
+            if (menu != null)
+            {
+                _context.Menulist.Remove(menu);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MenuExists(int id)
+        {
+          return (_context.Menulist?.Any(e => e.MenuId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/RoomsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Hotel_Management_System.Models;
+
+namespace Hotel_Management_System.Controllers
+{
+    public class RoomsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RoomsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rooms
+        public async Task<IActionResult> Index()
+        {
+              return _context.Roomlist != null ? 
+                          View(await _context.Roomlist.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Roomlist'  is null.");
+        }
+
+        // GET: Rooms/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Roomlist == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Roomlist
+                .FirstOrDefaultAsync(m => m.RoomId == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // GET: Rooms/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Rooms/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RoomId,RoomName,RoomType,RoomDescription,RoomPrice")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(room);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Roomlist == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Roomlist.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            return View(room);
+        }
+
+        // POST: Rooms/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RoomId,RoomName,RoomType,RoomDescription,RoomPrice")] Room room)
+        {
+            if (id != room.RoomId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.RoomId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(room);
+        }
+
+        // GET: Rooms/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Roomlist == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Roomlist
+                .FirstOrDefaultAsync(m => m.RoomId == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST: Rooms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Roomlist == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Roomlist'  is null.");
+            }
+            var room = await _context.Roomlist.FindAsync(id);
+            if (room != null)
+            {
+                _context.Roomlist.Remove(room);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomExists(int id)
+        {
+          return (_context.Roomlist?.Any(e => e.RoomId == id)).GetValueOrDefault();
+        }
+    }
+}
